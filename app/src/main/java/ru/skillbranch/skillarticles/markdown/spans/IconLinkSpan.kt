3d4517869fd47be14e95ed44bfe59b5ebb81610b
IldarUs <ildar__87@mail.ru>
2021-08-26 20:32:32 +0300

    2

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/IconLinkSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/IconLinkSpan.kt
@@ -11,52 +11,46 @@ import androidx.annotation.Px
 import androidx.annotation.VisibleForTesting
 
 class IconLinkSpan(
-    private val linkDrawable: Drawable,
-    @Px  val gap: Float,
-    @ColorInt
-    private val textColor: Int,
-    dotWidth: Float = 6f
+        private val linkDrawable: Drawable,
+        @Px
+        private val gap: Float,
+        @ColorInt
+        private val textColor: Int,
+        dotWidth: Float = 6f
 ) : ReplacementSpan() {
 
-    private var iconSize = 0
-    private var textWidth = 0f
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    var iconSize = 0
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    var textWidth = 0f
     private val dashs = DashPathEffect(floatArrayOf(dotWidth, dotWidth), 0f)
-
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     var path = Path()
 
     override fun draw(
-        canvas: Canvas,
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        x: Float,
-        top: Int,
-        y: Int,
-        bottom: Int,
-        paint: Paint
+            canvas: Canvas,
+            text: CharSequence,
+            start: Int,
+            end: Int,
+            x: Float,
+            top: Int,
+            y: Int,
+            bottom: Int,
+            paint: Paint
     ) {
         val textStart = x + iconSize + gap
         paint.forLine {
             path.reset()
-            path.moveTo(textStart, y + paint.descent())
-            path.lineTo(textStart + textWidth, y + paint.descent())
+            path.moveTo(textStart, bottom.toFloat())
+            path.lineTo(textStart + textWidth, bottom.toFloat())
             canvas.drawPath(path, paint)
         }
 
         canvas.save()
-        val transY = y + paint.descent() - linkDrawable.bounds.bottom
-        canvas.translate(x + gap / 2f, transY)
+        val trY = (bottom - linkDrawable.bounds.bottom.toFloat())
+        canvas.translate(x + gap / 2, trY)
         linkDrawable.draw(canvas)
         canvas.restore()
-
-        /*paint.forIcon {
-            canvas.save()
-            val trY = bottom - linkDrawable.bounds.bottom
-            canvas.translate(x, trY.toFloat())
-            linkDrawable.draw(canvas)
-            canvas.restore()
-        }*/
 
         paint.forText {
             canvas.drawText(text, start, end, textStart, y.toFloat(), paint)
@@ -65,62 +59,44 @@ class IconLinkSpan(
 
 
     override fun getSize(
-        paint: Paint,
-        text: CharSequence?,
-        start: Int,
-        end: Int,
-        fm: Paint.FontMetricsInt?
+            paint: Paint,
+            text: CharSequence?,
+            start: Int,
+            end: Int,
+            fm: Paint.FontMetricsInt?
     ): Int {
         if (fm != null) {
-            iconSize = fm.descent - fm.ascent //font size
+            iconSize = fm.descent - fm.ascent // fontSize
         }
         if (iconSize != 0) {
             linkDrawable.setBounds(0, 0, iconSize, iconSize)
             textWidth = paint.measureText(text.toString(), start, end)
-            //linkDrawable.setTint(iconColor)
         }
+
         return (iconSize + gap + textWidth).toInt()
     }
 
 
     private inline fun Paint.forLine(block: () -> Unit) {
-        //val oldColor = color
         val oldStyle = style
         val oldWidth = strokeWidth
 
-        strokeWidth = 0f
-        style = Paint.Style.STROKE
         pathEffect = dashs
         color = textColor
+        style = Paint.Style.STROKE
+        strokeWidth = 0f
 
         block()
 
         pathEffect = null
-        //color = oldColor
-        strokeWidth = oldWidth
         style = oldStyle
+        strokeWidth = oldWidth
     }
 
     private inline fun Paint.forText(block: () -> Unit) {
         val oldColor = color
-
         color = textColor
-
         block()
-
         color = oldColor
-    }
-
-    private inline fun Paint.forIcon(block: () -> Unit) {
-        val oldColor = color
-        val oldStyle = style
-
-        color = textColor
-        style = Paint.Style.STROKE
-
-        block()
-
-        color = oldColor
-        style = oldStyle
     }
 }
