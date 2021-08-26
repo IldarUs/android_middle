d8ef0ad8eded38029b856ddfa79226524c82b1c2
IldarUs <ildar__87@mail.ru>
2021-08-26 20:40:03 +0300

    3

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnderlineSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnderlineSpan.kt
@@ -8,8 +8,8 @@ import android.text.style.ReplacementSpan
 import androidx.annotation.VisibleForTesting
 
 class UnderlineSpan(
-    private val underlineColor: Int,
-    dotWidth: Float = 6f
+        private val underlineColor: Int,
+        dotWidth: Float = 6f
 ) : ReplacementSpan() {
     private var textWidth = 0
     private val dashs = DashPathEffect(floatArrayOf(dotWidth, dotWidth), 0f)
@@ -17,33 +17,53 @@ class UnderlineSpan(
     var path = Path()
 
     override fun draw(
-        canvas: Canvas,
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        x: Float,
-        top: Int,
-        y: Int,
-        bottom: Int,
-        paint: Paint
+            canvas: Canvas,
+            text: CharSequence,
+            start: Int,
+            end: Int,
+            x: Float,
+            top: Int,
+            y: Int,
+            bottom: Int,
+            paint: Paint
     ) {
-        //TODO implement me
+        paint.forLine {
+            path.reset()
+            path.moveTo(x, y + paint.descent())
+            path.lineTo(x + textWidth, y + paint.descent())
+            canvas.drawPath(path, paint)
+        }
+
+        canvas.drawText(text, start, end, x, y.toFloat(), paint)
     }
 
 
     override fun getSize(
-        paint: Paint,
-        text: CharSequence?,
-        start: Int,
-        end: Int,
-        fm: Paint.FontMetricsInt?
+            paint: Paint,
+            text: CharSequence?,
+            start: Int,
+            end: Int,
+            fm: Paint.FontMetricsInt?
     ): Int {
-        //TODO implement me
-        return 0
+        textWidth = paint.measureText(text.toString(), start, end).toInt()
+        return textWidth
     }
 
 
     private inline fun Paint.forLine(block: () -> Unit) {
-        //TODO implement me
+        val oldColor = color
+        val oldStyle = style
+        val oldWidth = strokeWidth
+
+        pathEffect = dashs
+        color = underlineColor
+        style = Paint.Style.STROKE
+        strokeWidth = 0f
+
+        block()
+
+        color = oldColor
+        style = oldStyle
+        strokeWidth = oldWidth
     }
 }
